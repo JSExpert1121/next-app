77c7d3bb29695a4d1f24080738d60a2dd066c6a1
JSExpert1121 <dage840128@outlook.com>
2021-06-30 08:14:15 +0300

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -19,6 +19,7 @@ class MyDocument extends Document {
 					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 					<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
 					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
 					<link rel="manifest" href="/manifest.json" />
 					<meta name="msapplication-TileColor" content="#ffffff" />
 					<meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
